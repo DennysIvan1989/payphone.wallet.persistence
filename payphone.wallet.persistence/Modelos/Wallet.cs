61e9eb5feaef8d606d2047558945c7a568651a79
Dennys Moyon <dennysmoyon@outlook.com>
2025-07-29 18:21:25 -0500

    actualizacion de entidades

--- payphone.wallet.persistence/Modelos/Wallet.cs
+++ payphone.wallet.persistence/Modelos/Wallet.cs
@@ -1,37 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace payphone.wallet.persistence.Modelos;
 
+[Table("Wallet")]
 public partial class Wallet
 {
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
 
+    [Column("documentId")]
+    [StringLength(10)]
+    [Unicode(false)]
     public string DocumentId { get; set; } = null!;
 
+    [Column("name")]
+    [StringLength(100)]
+    [Unicode(false)]
     public string Name { get; set; } = null!;
 
+    [Column("balance", TypeName = "decimal(18, 2)")]
     public decimal Balance { get; set; }
 
+    [Column("state")]
+    [StringLength(1)]
+    [Unicode(false)]
     public string? State { get; set; }
 
+    [Column("locks", TypeName = "decimal(18, 2)")]
     public decimal? Locks { get; set; }
 
+    [Column("calendarAt")]
     public DateTime CalendarAt { get; set; }
 
+    [Column("userCreate")]
+    [StringLength(20)]
+    [Unicode(false)]
     public string UserCreate { get; set; } = null!;
 
+    [Column("createAt")]
     public DateTime CreateAt { get; set; }
 
-    public string? UserUpdate { get; set; } = null!;
+    [Column("userUpdate")]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string? UserUpdate { get; set; }
 
+    [Column("updateAt")]
     public DateTime? UpdateAt { get; set; }
 
+    [Column("active")]
     public bool Active { get; set; }
 
+    [ForeignKey("UserCreate")]
+    [InverseProperty("WalletUserCreateNavigations")]
     public virtual UserW UserCreateNavigation { get; set; } = null!;
 
-    public virtual UserW UserUpdateNavigation { get; set; } = null!;
+    [ForeignKey("UserUpdate")]
+    [InverseProperty("WalletUserUpdateNavigations")]
+    public virtual UserW? UserUpdateNavigation { get; set; }
 
+    [InverseProperty("Wallet")]
     public virtual ICollection<WalletMovement> WalletMovements { get; set; } = new List<WalletMovement>();
 }
--- payphone.wallet.persistence/Modelos/WalletDbContext.cs
+++ payphone.wallet.persistence/Modelos/WalletDbContext.cs
@@ -22,112 +22,31 @@ public partial class WalletDbContext : DbContext
     {
         modelBuilder.Entity<UserW>(entity =>
         {
-            entity.HasKey(e => e.Cod);
-
-            entity.ToTable("UserW");
-
-            entity.Property(e => e.Cod)
-                .HasMaxLength(20)
-                .IsUnicode(false)
-                .HasColumnName("cod");
-            entity.Property(e => e.Active)
-                .HasDefaultValue(true)
-                .HasColumnName("active");
-            entity.Property(e => e.CreateAt).HasColumnName("createAt");
-            entity.Property(e => e.Email)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasColumnName("email");
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .IsUnicode(false)
-                .HasColumnName("name");
+            entity.Property(e => e.Active).HasDefaultValue(true);
         });
 
         modelBuilder.Entity<Wallet>(entity =>
         {
-            entity.ToTable("Wallet");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Active)
-                .HasDefaultValue(true)
-                .HasColumnName("active");
-            entity.Property(e => e.Balance)
-                .HasColumnType("decimal(18, 2)")
-                .HasColumnName("balance");
-            entity.Property(e => e.CalendarAt).HasColumnName("calendarAt");
-            entity.Property(e => e.CreateAt).HasColumnName("createAt");
-            entity.Property(e => e.DocumentId)
-                .HasMaxLength(10)
-                .IsUnicode(false)
-                .HasColumnName("documentId");
-            entity.Property(e => e.Locks)
-                .HasColumnType("decimal(18, 2)")
-                .HasColumnName("locks");
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .IsUnicode(false)
-                .HasColumnName("name");
-            entity.Property(e => e.State)
-                .HasMaxLength(1)
-                .IsUnicode(false)
-                .IsFixedLength()
-                .HasColumnName("state");
-            entity.Property(e => e.UpdateAt).HasColumnName("updateAt");
-            entity.Property(e => e.UserCreate)
-                .HasMaxLength(20)
-                .IsUnicode(false)
-                .HasColumnName("userCreate");
-            entity.Property(e => e.UserUpdate)
-                .HasMaxLength(20)
-                .IsUnicode(false)
-                .HasColumnName("userUpdate");
+            entity.Property(e => e.Active).HasDefaultValue(true);
+            entity.Property(e => e.State).IsFixedLength();
 
             entity.HasOne(d => d.UserCreateNavigation).WithMany(p => p.WalletUserCreateNavigations)
-                .HasForeignKey(d => d.UserCreate)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_UserW_Wallet");
 
-            entity.HasOne(d => d.UserUpdateNavigation).WithMany(p => p.WalletUserUpdateNavigations)
-                .HasForeignKey(d => d.UserUpdate)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_UserWu_Wallet");
+            entity.HasOne(d => d.UserUpdateNavigation).WithMany(p => p.WalletUserUpdateNavigations).HasConstraintName("FK_UserWu_Wallet");
         });
 
         modelBuilder.Entity<WalletMovement>(entity =>
         {
-            entity.ToTable("WalletMovement");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Active)
-                .HasDefaultValue(true)
-                .HasColumnName("active");
-            entity.Property(e => e.Amount)
-                .HasColumnType("decimal(18, 2)")
-                .HasColumnName("amount");
-            entity.Property(e => e.Available)
-                .HasColumnType("decimal(18, 2)")
-                .HasColumnName("available");
-            entity.Property(e => e.CalendarAt).HasColumnName("calendarAt");
-            entity.Property(e => e.CreateAt).HasColumnName("createAt");
-            entity.Property(e => e.Type)
-                .HasMaxLength(1)
-                .IsUnicode(false)
-                .IsFixedLength()
-                .HasColumnName("type");
-            entity.Property(e => e.UserCreate)
-                .HasMaxLength(20)
-                .IsUnicode(false)
-                .HasColumnName("userCreate");
-            entity.Property(e => e.WalletId).HasColumnName("walletId");
+            entity.Property(e => e.Active).HasDefaultValue(true);
+            entity.Property(e => e.Type).IsFixedLength();
 
             entity.HasOne(d => d.UserCreateNavigation).WithMany(p => p.WalletMovements)
-                .HasForeignKey(d => d.UserCreate)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_UserW_WalletMovement");
 
             entity.HasOne(d => d.Wallet).WithMany(p => p.WalletMovements)
-                .HasForeignKey(d => d.WalletId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Wallet_WalletMovement");
         });
